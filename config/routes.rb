73085fe319c5e970643da124f8fc37826b989c15
yoshikaneichimura <yoshikanemobile@gmail.com>
2023-09-05 12:46:50 +0000

    [Add] namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  devise_for :users,skip: [:passwords], controllers:{
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
