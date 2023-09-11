c46088ef90399a7ff787f722766708ea7b068be8
yoshikaneichimura <yoshikanemobile@gmail.com>
2023-09-11 13:25:29 +0000

    [Add] user sign_up sign_in sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about'
+  get 'homes/about' => 'homes#about', as: 'about'
 
   devise_for :users,skip: [:passwords], controllers:{
     registrations: "user/registrations",
