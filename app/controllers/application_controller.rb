89f8a5525cc7646fe919c8befd6505c339ceb302
yoshikaneichimura <yoshikanemobile@gmail.com>
2023-09-12 13:23:29 +0000

    [Add] admin sign_in sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    about_path
+    case resource
+    when Admin
+      root_path
+    when User
+      about_path
+    end
   end
 
   def after_sign_out_path_for(resource)
-    about_path
+    case resource
+    when :admin
+      root_path
+    when :user
+      about_path
+    end
   end
 
   protected
