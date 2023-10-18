3bb8c9e2bb9a7df1ea735fdea8c8a58f403ee374
yoshikaneichimura <yoshikanemobile@gmail.com>
2023-10-18 12:24:14 +0000

    [Add] review_create_action

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,16 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
+
+require("jquery")
+
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
