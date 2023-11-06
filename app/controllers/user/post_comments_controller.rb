bb7dcc6ce25aaa91a7767cc29f4bf797ac943e2b
yoshikaneichimura <yoshikanemobile@gmail.com>
2023-11-06 08:11:10 +0000

    [Fix] user_post_comment

--- app/controllers/user/post_comments_controller.rb
+++ app/controllers/user/post_comments_controller.rb
@@ -2,15 +2,15 @@ class User::PostCommentsController < ApplicationController
 
   def create
     @post_image = PostImage.find(params[:post_image_id])
-    comment = PostComment.new(post_comment_params)
-    comment.user_id = current_user.id
+    comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = @post_image.id
+    comment.save
     redirect_to user_post_image_path(@post_image)
   end
 
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:user_id,:post_image_id,:comment)
+    params.require(:post_comment).permit(:comment)
   end
 end
