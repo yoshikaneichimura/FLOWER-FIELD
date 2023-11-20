5da8ff044be60f36bf4c1154a19a9145505873e7
yoshikaneichimura <yoshikanemobile@gmail.com>
2023-11-20 06:23:59 +0000

    [Add] admin_post_image

---
+++ app/controllers/admin/post_images_controller.rb
@@ -0,0 +1,32 @@
+class Admin::PostImagesController < ApplicationController
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+  def edit
+    @post_image = PostImage.find(params[:id])
+  end
+
+  def update
+    post_image = PostImage.find(params[:id])
+    post_image.update(post_image_params)
+    redirect_to admin_post_image_path(post_image.id)
+  end
+
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to admin_post_images_path
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:user_id,:tag_id,:flower,:field,:day,:star,:address,:detail,:image)
+  end
+
+end
