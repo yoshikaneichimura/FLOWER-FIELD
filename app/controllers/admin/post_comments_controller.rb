8cf3d70dd8c78f6c0c066245ad955d266ca3f199
yoshikaneichimura <yoshikanemobile@gmail.com>
2023-12-11 06:11:18 +0000

    [Add] admin_post_comment

---
+++ app/controllers/admin/post_comments_controller.rb
@@ -0,0 +1,7 @@
+class Admin::PostCommentsController < ApplicationController
+
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to admin_post_image_path(params[:post_image_id])
+  end
+end
