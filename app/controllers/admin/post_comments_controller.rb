f3f4bbbd8438dcad2ec32deb909a6a386d83b231
yoshikaneichimura <yoshikanemobile@gmail.com>
2023-12-13 10:34:27 +0000

    [Fix] admin_page

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -2,6 +2,7 @@ class Admin::PostCommentsController < ApplicationController
 
   def destroy
     PostComment.find(params[:id]).destroy
+    flash[:notice] = "コメントを削除しました"
     redirect_to admin_post_image_path(params[:post_image_id])
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,6 +6,7 @@ class Admin::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @post_images = @user.post_images
   end
 
   def edit
