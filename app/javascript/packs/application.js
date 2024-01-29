1b88544a51d7805d07713087f84ff1876076a394
yoshikaneichimura <yoshikanemobile@gmail.com>
2024-01-29 06:14:26 +0000

    [Add] bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stlyesheets/application";
 
 Rails.start()
 // Turbolinks.start()
