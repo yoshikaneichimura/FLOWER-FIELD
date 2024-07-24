4d34fe3bd449d6da6fc1a7df79d9f49e49ed3327
yoshikaneichimura <yoshikanemobile@gmail.com>
2024-07-24 14:44:26 +0000

    [Add] user_test_data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-admin = Admin.new(:email => 'admin@test.com',:password => 'qqqqqq',:name => 'admin')
-admin.save!
+
+puts "seedの実行を開始"
+
+Admin.find_or_create_by!(email: "admin@test.com") do |admin|
+  admin.name = "admin"
+  admin.password = "password"
+end
 
 Tag.create([
   { area: '北海道'},
@@ -21,3 +26,45 @@ Tag.create([
   { area: '九州'},
   { area: '沖縄'},
   ])
+
+  User.find_or_create_by!(email: "guest@example.com") do |user|
+    user.name = "ゲストユーザー"
+    user.password = SecureRandom.urlsafe_base64
+  end
+
+  User.find_or_create_by!(email: "sakurai@test.com") do |user|
+    user.name = "桜井"
+    user.password = "password"
+    user.introduction = "特に花が好きというわけっじゃないんですけど、、、、皆さんのお役に立てる情報を投稿できるかと思って登録しました。"
+    user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+  end
+
+  User.find_or_create_by!(email: "lily@test.com") do |user|
+    user.name = "lily"
+    user.password = "password"
+    user.introduction = "花は太陽からの光線を自らのエネルギーにして生きている生物である為、光線に対して色彩、形態ともに緻密に計算された生物であると私は考えます。"
+    user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+  end
+
+  User.find_or_create_by!(email: "jack@test.com") do |user|
+    user.name = "JACK"
+    user.password = "password"
+    user.introduction = "花を撮ることよりも、花を描く得意とする者です。"
+    user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")
+  end
+
+  User.find_or_create_by!(email: "maikiko@test.com") do |user|
+    user.name = "魔鬼子"
+    user.password = "password"
+    user.introduction = "可愛いお花も良いのですが、さわればケガをしてしまいそうな強い花の方が私は好きです、よろしくお願いします。"
+    user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user5.jpg"), filename:"sample-user5.jpg")
+  end
+
+  # User.find_or_create_by!(email: "@test.com") do |user|
+  #   user.name = ""
+  #   user.password = "password"
+  #   user.introduction = ""
+  #   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user.jpg"), filename:"sample-user.jpg")
+  # end
+
+  puts "seedの実行が完了しました。"
