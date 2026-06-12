90cdca62f3186b46d2085fe407cf3c43e90ee0f4
yoshikaneichimura <yoshikanemobile@gmail.com>
2026-06-12 21:04:43 +0000

    [Update] search_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,33 +4,11 @@ class ApplicationController < ActionController::Base
 
   def set_search
     if user_signed_in?
-      @search = PostImage.ransack(params[:q])
-      if params[:q].present? && params[:q][:flower_or_field_or_day_or_address_or_detail_or_tag_area_cont].present?
-        key_words = params[:q][:flower_or_field_or_day_or_address_or_detail_or_tag_area_cont].split(/[\p{blank}\s]+/)
-        grouping_hash = key_words.reduce({}) do |hash, word|
-          hash.merge(word => { flower_or_field_or_day_or_address_or_detail_or_tag_area_cont: word})
-        end
-      end
-      @search = PostImage.ransack({
-        combinator: "and",
-        groupings: grouping_hash
-        })
-      @search_post_images = @search.result(distinct: true).page(params[:page]).sorted
+      @search_form = PostImage.ransack(params[:q])
+      @search_post_images = @search_form.result(distinct: true).page(params[:page]).sorted
     else admin_signed_in?
-      # @search = PostImage.ransack(params[:q])
-      # @search_post_images = @search.result(distinct: true).page(params[:page]).sorted
-       @search = User.ransack(params[:q])
-      if params[:q].present? && params[:q][:name_or_introduction_or_post_comments_comment_cont].present?
-        key_words = params[:q][:name_or_introduction_or_post_comments_comment_cont].split(/[\p{blank}\s]+/)
-        grouping_hash = key_words.reduce({}) do |hash, word|
-          hash.merge(word => { name_or_introduction_or_post_comments_comment_cont: word})
-        end
-      end
-      @search = User.ransack({
-        combinator: "and",
-        groupings: grouping_hash
-        })
-      @search_users = @search.result(distinct: true).page(params[:page]).sorted
+      @search_form = User.ransack(params[:q])
+      @search_users = @search_form.result(distinct: true).page(params[:page]).sorted
     end
   end
 
